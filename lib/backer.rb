d238ebf5c14eb89ae376697253036579d796990b
bennybop <bennybop74@yahoo.com>
2018-01-14 20:00:38 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,12 @@
 class Backer
-  @@backed_projects = []
-  attr_accessor :name
+  attr_reader :name, :backed_projects
 
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
   def back_project(project)
-    @@backed_projects << project 
+    backed_projects << project
+    project.backers << self 
   end 
 end
